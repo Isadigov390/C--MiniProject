281fa576eae777246eff45f286f461ee406a7d11
sadiqov2005 <isays@code.edu.az>
2024-10-20 16:57:20 +0400

    Empleyee class is done

--- HR-management-app.MiniProject/HR-management-app/Models/Employee.cs
+++ HR-management-app.MiniProject/HR-management-app/Models/Employee.cs
@@ -1,12 +1,60 @@
-﻿namespace HR_management_app.Models
+﻿using System.Threading.Channels;
+
+namespace HR_management_app.Models
 {
-    internal class Employee
+    public class Employee
     {
         public string No{ get; set; }
-        public string Fullname { get; set; }
-        public string Position { get; set; }
-        public decimal Salary { get; set; }
+        public string FullName { get; set; }
         public string DepartmentName { get; set; }
+        private decimal _salary;
+        private string _position;
+        private static int employeeCounter = 1000;
+        public string Position 
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                if (value.Length >= 2) _position = value;
+                else throw new Exception("Position must contain 2 characters ");
+            }
+        }
+
+        public decimal Salary {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+
+                if (value > 250) _salary = value;
+                else throw new Exception("Salary must be greater than 250");
+            }
+        }
+
+        public Employee(string fullName, string position, decimal salary, string departmentName)
+        {
+                
+            try
+            {
+                FullName = fullName;
+                Position = position;
+                Salary = salary;
+                DepartmentName = departmentName;
+
+                No = departmentName.Substring(0, 2).ToUpper() + employeeCounter;
+                employeeCounter++;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
 
     }
 }
--- HR-management-app.MiniProject/HR-management-app/Program.cs
+++ HR-management-app.MiniProject/HR-management-app/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using HR_management_app.Models;
+
+Employee employee = new Employee("ISa","m",11,"Ab");
